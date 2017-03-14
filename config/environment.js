1026d0351289b394ea0b08dec671b03e89d08e87
CristhGunners <gunners_one@hotmail.com>
2017-03-14 19:47:41 -0300

    full rename

--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'itunes',
+    modulePrefix: 'music-app',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
